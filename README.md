ae7b849bb9eaffd5f61059c9582c888d477b8150
ganggang <just_send@163.com>
2012-09-26 16:58:29 +0800

    初始化sns代码

--- README.md
+++
@@ -1,4 +0,0 @@
-ganggang.sns
-============
-
-sns
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sns::Application.config.secret_token = '56e0e6f03d7b3828727f752ef7430a93e67cda42693598d329d32e4da0e045b2a7be8b0091f633f1e7fbc13134c529301405170c697a1bed241b1b0e2d24ccaf'
